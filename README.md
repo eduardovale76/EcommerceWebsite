bf578813d3bd964a7e6c8667dd514149beb5cf74
Eduardo Vale <eduardovale76@gmail.com>
2022-08-03 20:20:40 -0300

    start Project

---
+++ README.md
@@ -0,0 +1,6 @@
+# EcomerceWebSite Study case
+
+Font: https://youtu.be/_ELCMngbM0E?list=PL-51WBLyFTg0omnamUjL1TCVov7yDTRng
+
+Fully functional ecommerce website with user and guest checkout capabilities.
+
---
+++ store/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import cart, checkout, store
+
+urlpatterns = [
+    path('', store, name='store'),
+    path('cart/', cart, name='cart'),
+    path('checkout', checkout, name='checkout'),
+]
